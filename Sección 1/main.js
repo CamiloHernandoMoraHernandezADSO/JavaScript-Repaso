6c563e4ee6e4abc3cdb69990c147670b5433958d
Camilo Mora ADSO <chmora8@misena.edu.co>
2022-12-06 11:49:33 -0500

    Sección 1 Ejercicio 3

--- Sección 1/main.js
+++ Sección 1/main.js
@@ -1,37 +1,22 @@
 addEventListener("DOMContentLoaded", (e) => {
-
-    let select = document.querySelector("#seleccion")
     let form = document.querySelector("#formulario")
     let result = document.querySelector("#resultado")
-    let valor
-    select.addEventListener("click", (e) => {
-        let myinput = document.querySelector('input[id="grado"]');
-
-        if (e.target.checked && e.target.value == "FAR") {
-            myinput.placeholder = "Ingrese Fahrenheit";
-        } else if (e.target.checked && e.target.value == "CEL") {
-            myinput.placeholder = "Ingrese Celsius";
-        }
-    })
     form.addEventListener("submit", (e) => {
         e.preventDefault()
         let dataInput = Object.fromEntries(new FormData(e.target));
-        console.log(dataInput)
-        if(dataInput.grados == "FAR"){
-            console.log(valor)
-            valor = (dataInput.grado - 32) / 1.8 
-            result.innerHTML = ""
+            console.log(dataInput)
+            tarea = dataInput.tarea 
+            hora = dataInput.hora
             result.insertAdjacentHTML("beforeend", `
-                <h1>${dataInput.grado + ' Fahrenheit son ' + valor + ' Celsius'}</h1>
+            <tr>
+                <td>
+                ${tarea}
+                </td>
+                <td>
+                ${hora}
+                </td>
+            </tr>
             `)
-        }
-        else if(dataInput.grados == "CEL"){
-            console.log(valor)
-            valor = dataInput.grado * 1.8 + 32
-            result.innerHTML = ""
-            result.insertAdjacentHTML("beforeend", `
-                <h1>${dataInput.grado + ' Celsius son ' + valor + ' Fahrenheit'}</h1>
-            `)
-        }
+        })
+
     })
-})
